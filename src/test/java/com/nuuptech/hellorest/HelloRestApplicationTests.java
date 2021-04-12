bc38784bea2db5b731e2706890c0178a69e5654c
therealthom <tmorenopl@gmail.com>
2021-04-12 17:19:34 -0500

    first commit

---
+++ src/test/java/com/nuuptech/hellorest/HelloRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuuptech.hellorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
