6518625e3c583781aa0d2566d99223e4dbe25623
Tomás Moreno Plaza <the.real.thom@gmail.com>
2021-07-21 15:32:28 -0500

    Update HelloRestController.java

--- src/main/java/com/nuuptech/hellorest/HelloRestController.java
+++ src/main/java/com/nuuptech/hellorest/HelloRestController.java
@@ -16,7 +16,7 @@ public class HelloRestController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		long c = counter.incrementAndGet();
 		double result = 0;		
-		System.out.print("Hola Ale - ");
+		//System.out.print("Hola");
 		for (int x = 0; x < 10; x++) {
 			result = factorial(20);
 			System.out.print(x + ", ");
