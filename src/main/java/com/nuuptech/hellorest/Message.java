e9db7f65642d585453b87a9da00c6499f4c0dd1e
Tom <the.real.thom@gmail.com>
2023-12-06 11:32:29 -0600

    Create Message.java

---
+++ src/main/java/com/nuuptech/hellorest/Message.java
@@ -0,0 +1,19 @@
+public class Message {
+    private String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    // Getters y setters
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
