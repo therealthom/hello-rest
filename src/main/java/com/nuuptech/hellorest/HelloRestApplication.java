dea0346918c381d2a27c1903cda36a0be1fc4402
Tomás Moreno Plaza <the.real.thom@gmail.com>
2023-12-06 11:42:40 -0600

    fix

--- src/main/java/com/nuuptech/hellorest/HelloRestApplication.java
+++ src/main/java/com/nuuptech/hellorest/HelloRestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloRestApplication.class, args);
 	}
